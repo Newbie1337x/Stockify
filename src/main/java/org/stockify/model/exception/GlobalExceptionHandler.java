cb781a7c26b58c3b8ef22e98defcda12cff7f699
Tomas cabrera <tomascabrera918@gmail.com>
2025-05-12 17:27:46 -0300

    proper merge

--- src/main/java/org/stockify/model/exception/GlobalExceptionHandler.java
+++ src/main/java/org/stockify/model/exception/GlobalExceptionHandler.java
@@ -3,57 +3,46 @@ package org.stockify.model.exception;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.mapping.PropertyReferenceException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.stockify.dto.response.ErrorResponse;
-import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
-
-
-
 
     @ExceptionHandler(DuplicatedUniqueConstraintException.class)
     public ResponseEntity<ErrorResponse> handleDuplicatedUniqueConstraintException(
             DuplicatedUniqueConstraintException ex,
             HttpServletRequest request
-    ) 
+    ) {
         return buildErrorResponse(HttpStatus.CONFLICT, ex, request);
     }
 
-
-    //Catch bad request on Sorts
+    // Catch bad request on Sorts
     @ExceptionHandler(PropertyReferenceException.class)
     public ResponseEntity<ErrorResponse> handleInvalidSort(PropertyReferenceException ex, HttpServletRequest request) {
-
         return buildErrorResponse(HttpStatus.BAD_REQUEST, ex, request);
     }
-
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundException(
             NotFoundException ex,
-            HttpServletRequest request
+            HttpServletRequest request){
         return buildErrorResponse(HttpStatus.NOT_FOUND, ex, request);
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponse> handleDataIntegrity(DataIntegrityViolationException ex,
-    HttpServletRequest request) {
-
+                                                             HttpServletRequest request) {
         return buildErrorResponse(HttpStatus.CONFLICT, ex, request);
     }
 
     private <T extends Throwable> ResponseEntity<ErrorResponse> buildErrorResponse(
             HttpStatus status, T ex, HttpServletRequest request) {
-
 
         ErrorResponse errorResponse = new ErrorResponse(
                 ex.getMessage(),
@@ -62,20 +51,6 @@ public class GlobalExceptionHandler {
                 request.getRequestURI(),
                 LocalDateTime.now()
         );
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse(ex, request));
+        return ResponseEntity.status(status).body(errorResponse);
     }
-
-    private <T extends Throwable> ErrorResponse errorResponse(T ex, HttpServletRequest request) {
-
-        logger.error("Error occurred: {}", ex.getMessage(), ex);
-
-        return new ErrorResponse(
-                ex.getMessage(),
-                ex.getClass().getSimpleName(),
-                HttpStatus.CONFLICT.value(),
-                request.getRequestURI(),
-                LocalDateTime.now()
-        );
-    }
-
 }
