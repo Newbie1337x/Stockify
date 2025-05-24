6ecaf96ab46a8000c64ebf4d53ca8b153e8f2699
Ignacio Dente <ignacioprogramando@gmail.com>
2025-05-24 00:22:18 -0300

    fix: update ShiftEntity class with necessary changes

--- src/main/java/org/stockify/model/entity/ShiftEntity.java
+++ src/main/java/org/stockify/model/entity/ShiftEntity.java
@@ -1,25 +1,36 @@
 package org.stockify.model.entity;
 
-
 import jakarta.persistence.*;
-
+import lombok.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "shifts")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ShiftEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "shift_id")
     private Long id;
-    @Column(name = "name")
-    private String name;
-    @Column(name = "entry_time")
+
+    @Column(name = "shift_day", nullable = false, length = 20)
+    private LocalDate day;
+
+    @Column(name = "entry_time", nullable = false, length = 20)
     private LocalDateTime entryTime;
-    @Column(name = "exit_time")
+
+    @Column(name = "exit_time", nullable = false, length = 20)
     private LocalDateTime exitTime;
+
     @OneToMany(mappedBy = "shiftEntity")
+
     private List<SessionPosEntity> posSessionEntities;
     @ManyToMany
     @JoinTable(
@@ -28,5 +39,4 @@ public class ShiftEntity {
             inverseJoinColumns = @JoinColumn(name = "employee_id") // columna que referencia a EmployeeEntity
     )
     private List<EmployeeEntity> employeeEntities;
-
 }
