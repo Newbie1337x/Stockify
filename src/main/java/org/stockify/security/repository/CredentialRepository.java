379d3e0a2c76d6954b94591b565369568bfc918c
Tomas cabrera <tomascabrera918@gmail.com>
2025-05-30 23:28:45 -0300

    Update CredentialRepository.java

--- src/main/java/org/stockify/security/repository/CredentialRepository.java
+++ src/main/java/org/stockify/security/repository/CredentialRepository.java
@@ -8,7 +8,5 @@ import java.util.Optional;
 @Repository
 public interface CredentialRepository extends JpaRepository<CredentialsEntity, Long> {
 
-    Optional<CredentialsEntity> findByEmail(String email);
-
     Optional<CredentialsEntity> findByUsername(String username);
 }
