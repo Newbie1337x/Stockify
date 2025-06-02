d4161f457dc64b9d3497f246f897b1eb4f0f34eb
joacoloool <joaquinalbornoz555@gmail.com>
2025-06-02 12:04:45 -0300

    feat(session-pos): add JPA Specifications
    Introduced the `SessionPosSpecifications` class to enable dynamic and composable filtering of POS sessions using Spring Data JPA Specifications.
    Swagger/OpenAPI annotations were introduced in this commit in SessionPosController
    
    This enhancement allows querying sessions based on multiple optional criteria such as:
    - Employee ID
    - POS ID
    - Opening and closing time ranges
    - Opening and closing amount thresholds
    - Cash difference thresholds
    - Session status (open/closed)
    
    Each specification returns null when the parameter is not provided, enabling clean and flexible predicate composition.
    
    No new endpoints were added in this commit, but this change prepares the service layer for advanced query capabilities and future integration with search/filter APIs.

--- src/main/java/org/stockify/model/repository/SessionPosRepository.java
+++ src/main/java/org/stockify/model/repository/SessionPosRepository.java
@@ -1,6 +1,7 @@
 package org.stockify.model.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.stockify.model.entity.SessionPosEntity;
 
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface SessionPosRepository extends JpaRepository<SessionPosEntity,Long> {
+public interface SessionPosRepository extends JpaRepository<SessionPosEntity,Long>, JpaSpecificationExecutor<SessionPosEntity> {
 
    Optional<SessionPosEntity> findByPosEntity_IdAndCloseTime(Long posEntityId, LocalDateTime closeTime);
 
