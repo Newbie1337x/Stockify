c6aaf63f5158e0c3291d2b716eab33bcc7fd92a9
joacoloool <joaquinalbornoz555@gmail.com>
2025-06-03 00:33:42 -0300

    fix: not found SessionPosRepo

---
+++ src/main/java/org/stockify/model/repository/SessionPosRepository.java
@@ -0,0 +1,18 @@
+package org.stockify.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+import org.stockify.model.entity.SessionPosEntity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+//repository
+@Repository
+public interface SessionPosRepository extends JpaRepository<SessionPosEntity,Long>, JpaSpecificationExecutor<SessionPosEntity> {
+
+    Optional<SessionPosEntity> findByPosEntity_IdAndCloseTime(Long posEntityId, LocalDateTime closeTime);
+
+    Boolean existsByPosEntity_IdAndCloseTime(Long posEntityId, LocalDateTime closeTime);
+}
