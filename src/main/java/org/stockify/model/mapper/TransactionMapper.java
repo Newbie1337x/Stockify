0b070c7845b3eceeeb006428035054debf614034
joacoloool <joaquinalbornoz555@gmail.com>
2025-06-07 04:19:08 -0300

    feat(transaction) add save method with Transaction
    Added the `save` method to `TransferService`, enabling the persistence of transfer operations. The method is annotated with `@Transactional` to ensure atomicity and data consistency during the transfer process.
    
    No new endpoints or Swagger/OpenAPI documentation were added in this commit.

--- src/main/java/org/stockify/model/mapper/TransactionMapper.java
+++ src/main/java/org/stockify/model/mapper/TransactionMapper.java
@@ -2,13 +2,14 @@ package org.stockify.model.mapper;
 
 import org.mapstruct.*;
 import org.stockify.dto.request.transaction.TransactionRequest;
+import org.stockify.dto.response.TransactionResponse;
 import org.stockify.model.entity.TransactionEntity;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper {
     TransactionEntity toEntity(TransactionRequest transactionRequest);
 
-    TransactionRequest toDto(TransactionEntity transactionEntity);
+    TransactionResponse toDto(TransactionEntity transactionEntity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     TransactionEntity partialUpdate(TransactionRequest transactionRequest, @MappingTarget TransactionEntity transactionEntity);
--- src/main/java/org/stockify/model/service/TransactionService.java
+++ src/main/java/org/stockify/model/service/TransactionService.java
@@ -1,17 +1,24 @@
 package org.stockify.model.service;
 
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.stockify.dto.request.transaction.TransactionRequest;
+import org.stockify.dto.response.TransactionResponse;
+import org.stockify.model.mapper.TransactionMapper;
 import org.stockify.model.repository.TransactionRepository;
-
+@Transactional
 @Service
 public class TransactionService {
     private final TransactionRepository transactionRepository;
+    private final TransactionMapper transactionMapper;
 
-    public TransactionService(TransactionRepository transactionRepository) {
+    public TransactionService(TransactionRepository transactionRepository, TransactionMapper transactionMapper) {
         this.transactionRepository = transactionRepository;
+        this.transactionMapper = transactionMapper;
     }
-
-    public Tran saveTransaction() {
+    @Transactional
+    public TransactionResponse saveTransaction(TransactionRequest request) {
+        return transactionMapper.toDto(transactionRepository.save(transactionMapper.toEntity(request)));
     }
 
 
