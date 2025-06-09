11862b2662ba920c80593a071160c27c5c48ae5e
joacoloool <joaquinalbornoz555@gmail.com>
2025-06-09 09:45:09 -0300

    fix

---
+++ src/main/java/org/stockify/dto/request/DetailTransactionRequest.java
@@ -0,0 +1,18 @@
+package org.stockify.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+import java.io.Serializable;
+
+/**
+ * DTO for {@link org.stockify.model.entity.DetailTransactionEntity}
+ */
+@Value
+@Getter
+@Setter
+public class DetailTransactionRequest{
+    String productBarcode;
+    Long quantity;
+}
--- src/main/java/org/stockify/model/service/TransactionService.java
+++ src/main/java/org/stockify/model/service/TransactionService.java
@@ -2,12 +2,18 @@ package org.stockify.model.service;
 
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
+import org.stockify.dto.request.DetailTransactionRequest;
 import org.stockify.dto.request.transaction.TransactionRequest;
+import org.stockify.dto.response.DetailTransactionResponse;
 import org.stockify.dto.response.TransactionResponse;
 import org.stockify.model.mapper.TransactionMapper;
 import org.stockify.model.repository.TransactionRepository;
-@Transactional
+
+import java.util.List;
+import java.util.Set;
+
 @Service
+@Transactional
 public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final TransactionMapper transactionMapper;
@@ -16,10 +22,29 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
         this.transactionMapper = transactionMapper;
     }
-    @Transactional
+
     public TransactionResponse saveTransaction(TransactionRequest request) {
-        return transactionMapper.toDto(transactionRepository.save(transactionMapper.toEntity(request)));
+        return transactionMapper
+                .toDto(transactionRepository
+                                .save(transactionMapper
+                                        .toEntity(request)));
     }
+
+    public TransactionResponse createTransaction(TransactionRequest request, Set<DetailTransactionRequest> detailTransactionRequests)
+    {
+        return null;
+
+    }
+
+    public List<TransactionResponse> findAll()
+    {
+        return transactionRepository
+                .findAll()
+                .stream()
+                .map(transactionMapper::toDto).toList();
+    }
+
+
 
 
 }
