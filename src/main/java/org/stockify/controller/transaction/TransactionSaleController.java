2f609b49537fe04a8398f59936ff10aaa15a59f2
Tomas cabrera <tomascabrera918@gmail.com>
2025-06-13 21:47:32 -0300

    Sale is now proper fixed, im going to kill myself

--- src/main/java/org/stockify/controller/transaction/TransactionSaleController.java
+++ src/main/java/org/stockify/controller/transaction/TransactionSaleController.java
@@ -3,10 +3,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,31 +16,41 @@ import org.stockify.dto.response.SaleResponse;
 import org.stockify.model.assembler.SaleModelAssembler;
 import org.stockify.model.service.SaleService;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("stores/{storeID}/pos/{posID}/transactions/sales")
 public class TransactionSaleController {
 
     private final SaleService saleService;
+    private final SaleModelAssembler saleModelAssembler;
 
 
-    public TransactionSaleController(SaleService saleService, SaleModelAssembler saleModelAssembler) {
-        this.saleService = saleService;
-
-    }
-
+    @Operation(
+            summary = "Create a new sale",
+            description = "Creates a sale for a given store and POS",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Sale created successfully", content = @Content(schema = @Schema(implementation = SaleResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid input data", content = @Content)
+            }
+    )
     @PostMapping
-    @Transactional
-    public ResponseEntity<SaleResponse> create(
-
+    public ResponseEntity<EntityModel<SaleResponse>> create(
+            @Parameter(description = "Sale request body", required = true)
             @Valid @RequestBody SaleRequest request,
+
+            @Parameter(description = "Store ID", required = true, example = "1")
             @PathVariable Long storeID,
+
+            @Parameter(description = "POS ID", required = true, example = "10")
             @PathVariable Long posID) {
-        System.out.println("Request: " + request);
         SaleResponse saleResponse = saleService.createSale(request, storeID, posID);
-        return new ResponseEntity<>(saleResponse,HttpStatus.CREATED);
+        EntityModel<SaleResponse> entityModel = saleModelAssembler.toModel(saleResponse);
+        return ResponseEntity
+                .created(entityModel.getRequiredLink("self").toUri())
+                .body(entityModel);
+
     }
-
-
-
-
 }
+
+
+
