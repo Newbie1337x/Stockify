6d8bc916d13580261a3328811a0297c5c9a4ae66
lucasmdv <lucasmdv11@gmail.com>
2025-06-16 13:12:10 -0300

    fix: update customer name retrieval in PdfGeneratorService and add SaleEntity relationship in TransactionEntity

--- src/main/java/org/stockify/model/service/PdfGeneratorService.java
+++ src/main/java/org/stockify/model/service/PdfGeneratorService.java
@@ -95,7 +95,7 @@ public class PdfGeneratorService {
             context.setVariable("date", dto.getDateTime());
             context.setVariable("total", dto.getTotal());
         } else {
-            String customerName = "";
+            String customerName = transaction.getSale().getClient().getFirstName() + " " + transaction.getSale().getClient().getLastName();
             context.setVariable("customerName", customerName);
             context.setVariable("transactionId", dto.getId());
             context.setVariable("storeName", dto.getStoreName());
